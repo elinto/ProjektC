d9a6235c6d6784d4c9b8d97817753806a08fb13e
AlexandraSoderlund <43433913+AlexandraSoderlund@users.noreply.github.com>
2018-10-26 14:48:57 +0200

    h

--- ProjektC/ProjektC/BLL/Podcast.cs
+++ ProjektC/ProjektC/BLL/Podcast.cs
@@ -28,5 +28,20 @@ namespace ProjektC.BLL
 
 
     }
+    public class Exceptions : Exception
+    {
+        public Exceptions()
+        {
+        }
 
+        public Exceptions(string message)
+            : base(message)
+        {
+        }
+
+        public Exceptions(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
     }
+}
